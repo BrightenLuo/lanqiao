e9a5a893e41d4264f480fecdd34adf1425ae9420
BrightenLuo <2636663928@qq.com>
2020-11-25 23:36:14 +0800

    11-25优化方法

--- newestHeroCode/AI/src/AI.java
+++ newestHeroCode/AI/src/AI.java
@@ -1,6 +1,7 @@
 import javax.microedition.lcdui.*;
 import javax.microedition.midlet.*;
 import java.io.*;
+
 public class AI extends MIDlet
 {
 	Display display;
@@ -19,94 +20,75 @@ public class AI extends MIDlet
 }
 class MainCanvas extends Canvas
 {
-	Image currentImg;//当前图片
-	Image[][] heroImg=new Image[4][3];//二维数组存图片
-	int x,y;//控制上下左右即坐标
-	int flag;//判断左右手的先后动作
-	int action;//接收按键动作
-	int oldAction;//存储上一个动作，用于判断转向时先立正
-	int first;//判断是否第一次执行转向
+	Image currentImg;
+	Image[][] heroMove=new Image[4][3];
+	int x,y;//坐标
+	int flag;//左右手脚互换标志
+	int oldAction;//记录上一个方向
+	int action;//转范围为成员变量
 	public MainCanvas(){
 		try{
-			/*
-			i means:
-			0:UP
-			1:DOWN
-			2:LEFT
-			3:RIGHT
+			/*			
+			i=0:UP			j=0:leftHand
+			i=1:DOWN		j=1:stand
+			i=2:LEFT		j=2:rightHand
+			i=3:RIGHT
 			*/
 
-			/*
-			j means:
-			0:left hand
-			1:stand
-			2:right hand
-			*/
+			for(int i=0;i<4;i++)
+				for(int j=0;j<3;j++)
+					heroMove[i][j]=Image.createImage("/sayo"+i+j+".png");
+					
+			currentImg=heroMove[1][1];
+			x=120;
+			y=100;
+			flag=-1;
+			oldAction=-1;
 
-			//二重循环读取图片
-			for(int i=0;i<heroImg.length;i++){
-				for(int j=0;j<heroImg[i].length;j++){
-					heroImg[i][j]=Image.createImage("/sayo"+i+j+".png");
-					}
-			}
-
-		     currentImg=heroImg[1][1];
-		     x=120;
-		     y=100;
-			 flag=0;
-			 oldAction=-1;
-			 first=-1;
-			 
 		}
 		catch(IOException e){
-			 e.printStackTrace();
+			e.printStackTrace();
 		}
 	}
-
 	public void paint(Graphics g){
-		g.setColor(250,220,190);
+		g.setColor(250,200,180);
 		g.fillRect(0,0,getWidth(),getHeight());
 		g.drawImage(currentImg,x,y,0);
 	}
 
 	//自定义方法
-	public void imageMove(int direction){
-		if(first==-1){     //第一次转向解决方案
-		currentImg=heroImg[direction][1];
-		first++;
-		}
-		else{
-			if(action!=oldAction)    //判断是否转向了，是的话先立正
-				currentImg=heroImg[direction][1];
+	public void heroAction(int direction){
+		if((flag==-1)&&(direction!=1))	//第一次转向，并判断它是不是在初始方向下移动
+			currentImg=heroMove[direction][1];
+		else if(action!=oldAction)
+			currentImg=heroMove[direction][1];
 			else{
 				if(flag%2==0)
-				currentImg=heroImg[direction][0];
+				currentImg=heroMove[direction][0];
 				else
-				currentImg=heroImg[direction][2];
-				}
-		}
-			switch(direction){
-				case 0: System.out.println("UP");y--;break;
-				case 1: System.out.println("DOWN");y++;break;
-				case 2: System.out.println("LEFT");x--;break;
-				case 3: System.out.println("RIGHT");x++;break;
+				currentImg=heroMove[direction][2];
 			}
+		switch(direction){
+			case 0:System.out.println("move UP!");y--;break;
+			case 1:System.out.println("move DOWN!");y++;break;
+			case 2:System.out.println("move LEFT!");x--;break;
+			case 3:System.out.println("move RIGHT!");x++;break;
+		}
 		repaint();
 		flag++;
 	}
-
 	public void keyPressed(int keyCode){
-		action=getGameAction(keyCode);
-		if(oldAction==-1)
-			oldAction=action;//第一次保存上一个动作
-	    if(action==UP)
-		   imageMove(0);//调用方法
-	    if(action==DOWN)
-		   imageMove(1);
-	    if(action==LEFT)
-		   imageMove(2);
-	    if(action==RIGHT)
-		   imageMove(3);
-	    oldAction=action;//把现动作变为旧动作
+		action=getGameAction(keyCode);//接收方向按键
+		if(oldAction==-1)	//第一次赋予oldAction值
+			oldAction=action;
+		if(action==UP)
+			heroAction(0);//调用方法
+		if(action==DOWN)
+			heroAction(1);
+		if(action==LEFT)
+			heroAction(2);
+		if(action==RIGHT)
+			heroAction(3);
+		oldAction=action;//将现动作变为上一个动作
 	}
 }
