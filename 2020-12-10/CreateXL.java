de1f11579743bb9532df9f01bb1201f93f315712
BrightenLuo <2636663928@qq.com>
2020-12-11 20:48:57 +0800

    通过pio创建并写入数据到本地的xls表格中，遇上Java编译不了中文或出现中文乱码的问题

---
+++ 2020-12-10/CreateXL.java
@@ -0,0 +1,53 @@
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import java.io.FileOutputStream;
+
+
+public class CreateXL {
+    
+    public static String outputFile = "D:\\test.xls";
+    public static void main(String argv[]) {
+        try {
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            
+            HSSFSheet sheet = workbook.createSheet("学生成绩");
+            
+            HSSFRow row = sheet.createRow((short)0);
+            HSSFRow row1=sheet.createRow((short)1);
+            HSSFRow row2=sheet.createRow((short)2);
+            HSSFRow row3=sheet.createRow((short)3);
+
+            
+            HSSFCell cell01 = row.createCell((short)0);
+            HSSFCell cell02 = row.createCell((short)1);
+            HSSFCell cell11=row1.createCell((short)0);
+            HSSFCell cell12=row1.createCell((short)1);
+            HSSFCell cell21=row2.createCell((short)0);
+            HSSFCell cell22=row2.createCell((short)1);
+            HSSFCell cell31=row3.createCell((short)0);
+            HSSFCell cell32=row3.createCell((short)1);
+            
+            
+            cell01.setCellValue("科目");
+            cell02.setCellValue("分数");
+            cell11.setCellValue("语文");
+            cell12.setCellValue("99");
+            cell21.setCellValue("数学");
+            cell22.setCellValue("100");
+            cell31.setCellValue("英语");
+            cell32.setCellValue("120");
+           
+            FileOutputStream fOut = new FileOutputStream(outputFile);
+            
+            workbook.write(fOut);
+            fOut.flush();
+            
+            fOut.close();
+            System.out.println("文件已生成！");
+        } catch (Exception e) {
+            System.out.println("xlCreate() : " + e);
+        }
+    }
+}
